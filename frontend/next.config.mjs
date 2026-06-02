0952fcf8ae3ba269f27d412d7635cf0e8614ac5b
Martinvb07 <martindavidvb@gmail.com>
2026-06-01 20:35:23 -0500

    fix(config-next): Imagenes BUILD

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,11 +2,9 @@
 const nextConfig = {
   images: {
     unoptimized: false,
-    quality: 100,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [256, 384, 512, 720, 1080, 1440],
     formats: ['image/webp'],
   },
 };
-
 export default nextConfig;
